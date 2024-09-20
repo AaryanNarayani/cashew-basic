f27bd3bd915453b60d35d95a0c9e6660a39045a5
AaryanNarayani <aaryannarayani2004@gmail.com>
2024-09-21 00:40:14 +0530

    cleanup

--- server/dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DECIMAL = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = 'secret-cashew';
-exports.DECIMAL = 100;
